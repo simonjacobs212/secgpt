3a14e5672ebe7411b664b52c80e343969c957d65
Simon Adler Jacobs <simonadlerjacobs@gmail.com>
2025-06-10 09:58:19 -0400

    cleaned up varaibles

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const axios = require('axios');
 const app = express();
 app.use(express.json());
 
-const SLACK_WEBHOOK_URL = process.env.https://hooks.slack.com/services/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;
+const SLACK_WEBHOOK_URL = process.env.SLACK_WEBHOOK_URL;
 
 app.post('/slack', async (req, res) => {
   try {
